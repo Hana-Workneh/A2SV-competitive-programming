d9a1a1eca64e7c2093f2d1ec78ca82ca4c918a4b
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2021-12-13 06:48:36 -0800

    Create domino-piling.cpp

---
+++ day-1/domino-piling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int m, n, num;
+    cout<<"enter your number";
+    cin>>m;
+    cin>>n;
+    num= (m*n)/2;
+    cout<<endl<<num;
+    return num;
+}
