63c811805c796cbc4f87f6ceb390226d23d4b2d3
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-03-04 11:41:23 +0300

    Time: 172 ms (94.43%), Space: 18.5 MB (97.79%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -5,7 +5,6 @@ class Solution:
             for j in range(len(matrix[i])):
                 nums.append(matrix[i][j])
         nums.sort()
-        print(nums)
         while len(nums) > k :
             nums.pop()
         return nums[-1]
