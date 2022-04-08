3868c040e7598b180a568bd3761578fb55ccee47
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-04-08 14:25:17 +0300

    Time: 224 ms (51.21%), Space: 14.4 MB (33.53%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,15 +1,22 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if n == 0: 
-            return True
-        N = len(flowerbed)
-        for i, f in enumerate(flowerbed):
-            if (i == 0 and i + 1 >= N and flowerbed[i] == 0) or \
-            (i == 0 and i + 1 < N and flowerbed[i + 1] == 0 and flowerbed[i] == 0) or \
-            (i - 1 >= 0 and i + 1 < N and flowerbed[i + 1] == 0 and flowerbed[i - 1] == 0 and flowerbed[i] == 0) or \
-            (i == N - 1 and flowerbed[i - 1] == 0 and flowerbed[i] == 0):
-                n -= 1
-                flowerbed[i] = 1
-            if n == 0:
-                return True
-        return False
+        count, N = 0, len(flowerbed)       
+        if N == 1:
+            if flowerbed[0]==0:
+                count += 1
+                n  -= 1
+            
+        else:
+            if flowerbed[1] == flowerbed[0] == 0:
+                flowerbed[0] = 1
+                count += 1
+            if flowerbed[N - 2] == flowerbed[N - 1] == 0:
+                flowerbed[N - 1] = 1
+                count += 1
+                
+            for i in range(1, N - 1):
+                if flowerbed[i] == flowerbed[i + 1] == flowerbed[i - 1] == 0:
+                    flowerbed[i] = 1
+                    count += 1
+                    
+        return count >= n
