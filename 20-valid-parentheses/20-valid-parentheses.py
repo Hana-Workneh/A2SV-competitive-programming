698b7acca451ad0531c5cd90390864b7dc201490
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-07-22 02:44:26 +0300

    Time: 48 ms (51.23%), Space: 13.8 MB (97.84%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par=dict()
+        par["("]=')'
+        par['{']='}'
+        par['[']=']'
+        stack = []
+        
+        for i in s:
+            if i in par.keys():
+                stack.append(i)
+            elif len(stack) == 0 or i != par[stack.pop()]:
+                return False
+            
+        return len(stack) == 0
