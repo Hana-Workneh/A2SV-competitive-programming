f3fccab02cbd1615e89b165e03c8e51442c49171
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-09 17:04:22 +0300

    Time: 191 ms (7.54%), Space: 14.3 MB (11.65%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -3,11 +3,13 @@ class Solution:
         rn = list(ransomNote)
         mg = list(magazine)
         count = 0
+        
         for i in range(len(rn)):
             if rn[i] in mg:
                 ind= mg.index(rn[i])
                 mg.pop(ind)
                 count += 1
+                
         if len(rn) == count:
             return True
         return False
