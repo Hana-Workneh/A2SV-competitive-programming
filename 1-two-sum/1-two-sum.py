d6e16d856ff0fbef62b1b945a2b91ec501e3ffc8
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-10 22:30:59 +0300

    Time: 71 ms (87.45%), Space: 15.2 MB (50.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}  
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j] + nums[i] == target:
-                    return [i, j]
+            diff = target - nums[i]
+            if diff in hashMap:
+                return [i, hashMap[diff]]
+            hashMap[nums[i]] = i
