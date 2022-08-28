e94da87f14ec9fcebc455f021bd16ce199d2bc68
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-28 22:35:38 +0300

    Time: 93 ms (86.58%), Space: 14.2 MB (76.31%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,15 +1,39 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        d = defaultdict(list)
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                d[i - j].append(mat[i][j])
+        rows = len(mat)
+        cols = len(mat[0])
+        for i in range(rows):
+            nums = []
+            k = i
+            j = 0
+            while k < rows and j < cols:
+                nums.append(mat[k][j])
+                j += 1
+                k += 1
+            # print(nums)
+            nums = sorted(nums)
+            k = i
+            j = 0
+            while k < rows and j < cols:
+                mat[k][j] = nums[j]
+                j += 1
+                k += 1
         
-        for k in d:
-            d[k].sort(reverse=True)
-        
-
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                mat[i][j] = d[i - j].pop()
+        for i in range(1, cols):
+            nums = []
+            k = 0
+            j = i
+            while k < rows and j < cols:
+                nums.append(mat[k][j])
+                j += 1
+                k += 1
+            # print(nums)
+            nums = sorted(nums)
+            k = 0
+            j = i
+            while k < rows and j < cols:
+                mat[k][j] = nums[k]
+                j += 1
+                k += 1
+                
         return mat
