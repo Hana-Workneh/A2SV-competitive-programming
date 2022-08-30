6970eb54a60cff37568ae981f9f547b0050b32a4
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-30 22:11:06 +0300

    Time: 62 ms (34.00%), Space: 14 MB (29.99%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        row = 0
+        col = 0
+        
+        for _ in range(n//2):
+            col_boundary = n - col - 1
+            while col < col_boundary:
+               
+                matrix[row][col], matrix[col][n-1-row] = matrix[col][n-1-row],\
+                matrix[row][col]
+                
+                matrix[row][col], matrix[n-1-row][n-1-col] = \
+                matrix[n-1-row][n-1-col], matrix[row][col]
+
+                matrix[row][col], matrix[n-1-col][row]  = matrix[n-1-col][row],\
+                matrix[row][col]
+                
+                col += 1
+            
+            row += 1
+            col = row
