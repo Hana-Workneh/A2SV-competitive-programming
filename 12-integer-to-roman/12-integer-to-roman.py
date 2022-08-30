3a5a3c7870239db106ecd1dc04ae73e015e373e2
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-31 00:01:26 +0300

    Time: 75 ms (58.51%), Space: 14 MB (35.57%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,16 +1,11 @@
+
+val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
 class Solution:
-    roman_number = ''
-
-    def intToRoman(self, num: int) -> str:
-        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
-        symbols = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
-        
-        for value, symbol in zip(values, symbols):
-            num = self.convertNumToLetter(num, value, symbol)
-
-        return self.roman_number
-        
-    def convertNumToLetter(self, num: int, value: int, symbol: str) -> int:
-        out = num // value
-        self.roman_number += symbol * out
-        return num - (out * value)
+    def intToRoman(self, N: int) -> str:
+        ans = ""
+        for i in range(13):
+            while N >= val[i]:
+                ans += rom[i]
+                N -= val[i]
+        return ans
