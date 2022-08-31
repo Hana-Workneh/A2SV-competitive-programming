39b9054d2020de49311607496a8ced77d03c05ce
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-08-31 23:07:44 +0300

    Time: 28 ms (97.52%), Space: 13.9 MB (55.28%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,6 +1,15 @@
-class Solution(object):
-    def isPerfectSquare(self, num):
-        x = num**0.5
-        if(int(x) == x):
-            return True
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        # Binary search solution
+        l, r = 0, num
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid == num:
+                return True
+            else:
+                if mid * mid > num:
+                    r = mid - 1
+                else:
+                    l = mid + 1
         return False
+    
