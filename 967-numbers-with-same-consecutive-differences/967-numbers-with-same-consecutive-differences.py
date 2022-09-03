c1f3291da84fa2a144adcbc825fb1f34f82f48e9
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-09-03 23:16:57 +0300

    Time: 37 ms (96.68%), Space: 14.2 MB (41.53%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,19 +1,26 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-
-        ans = []
-
-        def getNumbers(size, x, num):
-            if size == 0:
-                ans.append(num)
+        res = []
+        
+        def comb(num, leni):
+            if leni == n:
+                res.append(num)
                 return
-
-            if x + k < 10:
-                getNumbers(size - 1, x + k, (num * 10) + (x + k))
-            if k != 0 and x - k >= 0:
-                getNumbers(size - 1, x - k, (num * 10) + (x - k))
-
+            
+            if k == 0:
+                comb(num * 10 + num % 10, leni + 1)
+                return
+            
+            if num % 10 + k <= 9:
+                comb(num * 10 + num % 10 + k, leni + 1)
+                
+            if num % 10 - k >= 0:
+                comb(num * 10 + num % 10 - k, leni + 1)
+                
+            return
+        
         for i in range(1, 10):
-            getNumbers(n - 1, i, i)
-
-        return ans
+            comb(i, 1)
+            
+        return res
+        
