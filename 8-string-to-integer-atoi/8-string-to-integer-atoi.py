5a1ac4f643fef4828a90faf5da87be33a6897c85
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-09-07 22:24:02 +0300

    Time: 35 ms (95.31%), Space: 13.9 MB (28.64%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,28 +1,24 @@
 class Solution:
+
     def myAtoi(self, s: str) -> int:
-        sign = 1
+        def check( temp_res: int):
+            if temp_res > (2 ** 31) - 1:
+                return (2 ** 31) - 1
+            elif temp_res < (-2 ** 31):
+                return (-2 ** 31)
+            else:
+                return temp_res
         s = s.strip()
-        if s == "" or s =="0":
+        if len(s) == 0:
             return 0
-        new_s = ""
-        if s[0] == "-":
-            sign = -1
-            new_s = s[1:]
-        elif s[0] == "+":
-            new_s = s[1:]
-        else:
-            new_s = s
-        if new_s == "" or new_s[0] == "+" or new_s[0] == "-" or not new_s[0].isnumeric() :
-            return 0
-        numbers = ""    
-        for num in new_s:
-            if num.isnumeric():
-                numbers += num
+        flag = -1 if s[0] == "-" else 1
+        s = s[1:] if (s[0] == "-" or s[0] == "+") else s
+        res=0
+        for i in s:
+            if "0" <= i <= "9":
+                res *= 10
+                res+=int(i)
+                res = flag*check(flag*res)
             else:
                 break
-        if int(numbers) * sign > 2**31 -1:
-            return 2**31 -1
-        elif int(numbers) *sign < - 2**31:
-            return  2**31 * sign
-
-        return int(numbers) * sign
+        return check(flag*res)
