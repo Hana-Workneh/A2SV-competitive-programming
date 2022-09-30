eb43eb856c5371b491ea3fc77b717226bf8caee1
Hana Workneh <70158272+Hana-Workneh@users.noreply.github.com>
2022-09-30 07:44:06 +0300

    Time: 141 ms (87.92%), Space: 20.2 MB (21.63%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,16 +1,18 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        events = [(L, -H, R) for (L, R, H) in buildings]
-        events += [(R, 0, 0) for (_, R, _) in buildings]
-        events.sort()
-        
-        ans = [[0, 0]]
-        live  = [[0, float("inf")]]
-        for (L, negH, R) in events:
-            while live[0][1] <= L:
-                heapq.heappop(live)
-            if negH:
-                heapq.heappush(live, [negH, R])
-            if ans[-1][1] != -live[0][0]:
-                ans.append([L, -live[0][0]])
-        return ans[1:]
+        heights = []
+        for start, end, height in buildings:
+            heights.append([start, -height])
+            heights.append([end, height])
+        heights = sorted(heights)
+        b, ans = SortedList([0]), []
+        for x, h in heights:
+            if h > 0:
+                b.remove(h)
+            else:
+                b.add(-h)
+            if not ans or ans[-1][1] != b[-1]:
+                ans.append([x, b[-1]])            
+        return ans
